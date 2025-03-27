1eb2d919e69247cb9b8a591e1688194f4c533de8
Seth Juarez <me@sethjuarez.com>
2025-03-27 17:17:13 +0100

    default talk track prompty files

---
+++ 3-agent/agent.py
@@ -0,0 +1,21 @@
+import prompty
+from prompty.tracer import Tracer, trace, PromptyTracer
+
+tracy = PromptyTracer()
+Tracer.add("prompty", tracy.tracer)
+
+@trace
+def get_current_weather(city:str, unit: str = "Celsius"):
+    return f"The weather in {city} is 32 {unit}"
+
+@trace
+def execute_agent():
+    p = prompty.load("agent.prompty")
+    # set the tool function
+    p.set_tool_value("get_current_weather", get_current_weather)
+    result = prompty.execute(p, inputs={"question": "What's the weather like in Japan and Mexico?"}, merge_sample=True)
+    return result
+
+if __name__ == "__main__":
+    result = execute_agent()
+    print(result)
